7b8a81f426436dc6c2541d9fc94d41de5a8124f3
Disha Jadav <disha.jadav@neuralweb.tech>
2025-02-17 22:53:56 +0530

    Initial set-up nextjs and shadcn ui

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+export const metadata = {
+  title: "Home",
+}
+const HomePage = () => {
+  return ( <>
+  hellii
+  <Button>Dishaa</Button>
+  </> );
+}
+ 
+export default HomePage;
