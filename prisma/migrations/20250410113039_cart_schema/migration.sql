cd856fdd978ad3f24bc87883cf048be9604edadb
Disha Jadav <dishajadav12402@gmail.com>
2025-04-10 17:07:23 +0530

    added cart schema to the db and validate it

---
+++ prisma/migrations/20250410113039_cart_schema/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "public"."Cart" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "userId" UUID,
+    "sessionCartId" TEXT NOT NULL,
+    "items" JSON[] DEFAULT ARRAY[]::JSON[],
+    "itemsPrice" DECIMAL(12,2) NOT NULL,
+    "totalPrice" DECIMAL(12,2) NOT NULL,
+    "shippingPrice" DECIMAL(12,2) NOT NULL,
+    "taxPrice" DECIMAL(12,2) NOT NULL,
+    "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Cart_pkey" PRIMARY KEY ("id")
+);
